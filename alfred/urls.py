4ef4a334489d5de57d0094c3ab6a1e75f2ff9942
Bart Feenstra <bart@mynameisbart.com>
2017-04-24 19:31:45 +0200

    Simplify the GraphQL interface and re-allow batching (#4)
    * Remove outdated tools.
    
    * Simplify the GraphQL interface and re-allow batching.

--- alfred/urls.py
+++ alfred/urls.py
@@ -3,5 +3,5 @@ from alfred.graphql import Schema
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    url(r'^$', GraphQLView.as_view(schema=Schema())),
+    url(r'^$', GraphQLView.as_view(schema=Schema(), batch=True)),
 ]
