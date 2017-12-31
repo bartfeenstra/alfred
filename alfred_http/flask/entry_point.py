6daa113bdee09640440d3b7035ad817e9b79df17
Bart Feenstra <bart@mynameisbart.com>
2017-12-31 19:00:19 +0100

    Make the extensions configurable for the HTTP app.

--- alfred_http/flask/entry_point.py
+++ alfred_http/flask/entry_point.py
@@ -1,11 +1,15 @@
+import importlib
+import sys
+
 from alfred.app import App
-from alfred_openapi.extension import OpenApiExtension
-from alfred_rest.extension import RestExtension
-from alfred_rest.tests import RestTestExtension
+from alfred_http.extension import HttpExtension
 
 alfred = App()
-# @todo Make the extensions configurable.
-alfred.add_extension(OpenApiExtension)
-alfred.add_extension(RestExtension)
-alfred.add_extension(RestTestExtension)
+alfred.add_extension(HttpExtension)
+for qualname in sys.argv[1:]:
+    module_name, class_name = qualname.rsplit('.', 1)
+    importlib.import_module(module_name)
+    module = sys.modules[module_name]
+    extension = getattr(module, class_name)
+    alfred.add_extension(extension)
 app = alfred.service('http', 'flask')
