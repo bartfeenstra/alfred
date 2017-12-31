883cd54de77ad131615224546ca5969f5bb7f56b
Bart Feenstra <bart@mynameisbart.com>
2017-12-31 10:32:57 +0100

    Remove traces of unknown modules.

--- alfred_http/flask/entry_point.py
+++ alfred_http/flask/entry_point.py
@@ -1,5 +1,4 @@
 from alfred.app import App
-from alfred_device.extension import DeviceExtension
 from alfred_openapi.extension import OpenApiExtension
 from alfred_rest.extension import RestExtension
 from alfred_rest.tests import RestTestExtension
@@ -9,5 +8,4 @@ alfred = App()
 alfred.add_extension(OpenApiExtension)
 alfred.add_extension(RestExtension)
 alfred.add_extension(RestTestExtension)
-alfred.add_extension(DeviceExtension)
 app = alfred.service('http', 'flask')
