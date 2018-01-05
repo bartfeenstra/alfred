67fce22a4f381e2c8d1a24d1c53932f9213d1957
Bart Feenstra <bart@mynameisbart.com>
2018-01-05 14:20:27 +0100

    Allow dispatchers to reduce the handler results to single values.

--- alfred/dispatch.py
+++ alfred/dispatch.py
@@ -11,19 +11,35 @@ class Dispatcher:
     @contract
     def __init__(self, dispatched: Callable, reducer=None):
         update_wrapper(self, dispatched)
+        self._handler_self = None
+        self._handler_cls = None
         self._handlers = [dispatched]
         self._reducer = reducer
 
     def __get__(self, obj, objtype):
-        arg = obj if obj else objtype
-        return partial(self.__call__, arg)
+        self._handler_self = obj
+        self._handler_cls = objtype
+        return self
 
     def __call__(self, *args, **kwargs):
+        handlers = self._handlers
+
+        # Keep those handlers that are on the same instance or class.
+        if self._handler_cls:
+            handlers = filter(lambda x: hasattr(self._handler_cls, x.__name__) and (x == getattr(
+                self._handler_cls, x.__name__) or isinstance(getattr(self._handler_cls, x.__name__), Dispatcher)), handlers)
+
+            # Apply the instance or class.
+            arg = self._handler_self if self._handler_self else self._handler_cls
+            handlers = map(lambda x: partial(x, arg), handlers)
+
+        # Invoke all handlers and reduce the results.
         if self._reducer:
-            results = map(lambda h: h(*args, **kwargs), self._handlers)
+            results = map(lambda h: h(*args, **kwargs), handlers)
             return reduce(self._reducer, results)
+        # Invoke all handlers and ignore the results.
         else:
-            for handler in self._handlers:
+            for handler in handlers:
                 handler(*args, **kwargs)
             return None
 
--- alfred/tests/test_dispatch.py
+++ alfred/tests/test_dispatch.py
@@ -4,52 +4,96 @@ from unittest import TestCase
 from alfred.dispatch import dispatch
 
 
+class SomeClass:
+    def __init__(self):
+        self._results = []
+
+    @dispatch()
+    def foo(self):
+        self._results.append('foo')
+
+    @foo.register()
+    def handler1(self):
+        self._results.append('handler1')
+
+    @foo.register()
+    def handler2(self):
+        self._results.append('handler2')
+
+
+class SomeChildClass(SomeClass):
+    @SomeClass.foo.register()
+    def handler3(self):
+        self._results.append('someChildHandler')
+
+
+class SomeOtherChildClass(SomeClass):
+    @SomeClass.foo.register()
+    def handler4(self):
+        self._results.append('someOtherChildHandler')
+
+
 class InstanceMethodDispatchTest(TestCase):
-    class SomeClass:
-        def __init__(self):
-            self._results = []
-
-        @dispatch()
-        def foo(self):
-            self._results.append('foo')
-            return 'fooResult'
-
-        @foo.register()
-        def handler1(self):
-            self._results.append('handler1')
-            return 'handler1Result'
-
-        @foo.register()
-        def handler2(self):
-            self._results.append('handler2')
-            return 'handler2Result'
-
     def testDispatch(self):
-        sut = self.SomeClass()
+        sut = SomeClass()
         result = sut.foo()
         self.assertEquals(result, None)
         self.assertEquals(sut._results, ['foo', 'handler1', 'handler2'])
 
+    def testDispatchExecutesOwnedHandlersOnly(self):
+        """
+        Ensures method handlers on other classes are not invoked.
+        :return:
+        """
+        sut = SomeChildClass()
+        result = sut.foo()
+        self.assertEquals(result, None)
+        self.assertEquals(
+            sut._results, ['foo', 'handler1', 'handler2', 'someChildHandler'])
+
+
+class SomeReducedClass:
+    @dispatch(reducer=operator.concat)
+    def foo(self):
+        return ['fooResult']
+
+    @foo.register()
+    def handler1(self):
+        return ['handler1Result']
+
+    @foo.register()
+    def handler2(self):
+        return ['handler2Result']
+
+
+class SomeReducedChildClass(SomeReducedClass):
+    @SomeReducedClass.foo.register()
+    def handler3(self):
+        return ['someReducedChildHandler']
+
+
+class SomeReducedOtherChildClass(SomeReducedClass):
+    @SomeReducedClass.foo.register()
+    def handler4(self):
+        return ['someReducedOtherChildHandler']
+
 
 class ReducedInstanceMethodDispatchTest(TestCase):
-    class SomeClass:
-        @dispatch(reducer=operator.concat)
-        def foo(self):
-            return ['fooResult']
-
-        @foo.register()
-        def handler1(self):
-            return ['handler1Result']
-
-        @foo.register()
-        def handler2(self):
-            return ['handler2Result']
-
     def testDispatch(self):
-        sut = self.SomeClass()
+        sut = SomeReducedClass()
         result = sut.foo()
-        self.assertEquals(
-            result, ['fooResult', 'handler1Result', 'handler2Result'])
+        self.assertEquals(result,
+                          ['fooResult', 'handler1Result', 'handler2Result'])
+
+    def testDispatchExecutesOwnedHandlersOnly(self):
+        """
+        Ensures method handlers on other classes are not invoked.
+        :return:
+        """
+        sut = SomeReducedChildClass()
+        result = sut.foo()
+        self.assertEquals(result, [
+                          'fooResult', 'handler1Result', 'handler2Result', 'someReducedChildHandler'])
 
 
 @dispatch(reducer=operator.concat)
@@ -68,7 +112,6 @@ def handler2():
 
 
 class ReducedFunctionDispatchTest(TestCase):
-
     def testDispatch(self):
         result = foo()
         self.assertEquals(
